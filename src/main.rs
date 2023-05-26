f1f5035a7a96a248aa3fa240904ed2f4e015c1dd
agnxsh <iamagnix@gmail.com>
2023-05-27 01:48:42 +0530

    chore: main, preparing cli tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use trishul::{Contract::Contract, queryeth, flag};
+
+#[derive(Parser)]
+#[command(author, version, about, longg_about = None)]
+#[command(propagate_version = true)]
+
+struct Clitool {
+
+}
+
+fn main (){
+    let cli = Clitool::parse();
+    let rt = tokio::runtime::Runtime::new().unwrap();
 }
