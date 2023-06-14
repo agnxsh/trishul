d8f9585c071b33b35c69f15d640d6c8b88e34dcb
agnxsh <iamagnix@gmail.com>
2023-06-14 20:26:58 +0530

    feat: contract parsing logic

--- src/contract_handler.rs
+++ src/contract_handler.rs
@@ -2,6 +2,7 @@ use ethers::types::Bytes;
 pub struct Contract {
     pub bytecode: Bytes,
 }
+///wrapper type around Bytes to deserialize/serialize "0x" prefixed ethereum to strings
 
 impl Contract {
     pub fn new(mut self, bytecode: Bytes) -> Self {
@@ -10,6 +11,34 @@ impl Contract {
     }
 
     pub fn get_smart_contract_dispatcher(self, design_pattern: Vec<u8>){
-        
+        search(design_pattern, self.bytecode);
+    }
+
+}
+
+///computing the pattern array first
+fn compute_pattern_array(design_pattern: &Vec<u8>, m : usize) -> Vec<i32>
+
+///helper function to search for function signatures from the bytecode
+fn search (design_pattern: Vec<u8>, bytecode: Bytes) {
+    let m = design_pattern.iter().len();
+    let n = bytecode.iter().len();
+
+    let mut i = 0;
+    let mut j = 0;
+
+    let mut signature : Bytes = Bytes::from(bytecode[0 as usize..1 as usize].to_vec());
+
+    let pattern_array : Vec<i32> = compute_pattern_array(&design_pattern, m);
+
+    loop {
+        if n-1 < m - j {
+            break;
+        }
+
+        if design_pattern[j] = bytecode[i]{
+            if design_pattern[j] == 0x63
+
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,27 @@ use trishul::{Contract::Contract, queryeth, flag};
 #[command(propagate_version = true)]
 
 struct Clitool {
-
+    #[command(subcommand)]
+    dispatcher: sub :: Commands
 }
 
 fn main (){
     let cli = Clitool::parse();
     let rt = tokio::runtime::Runtime::new().unwrap();
+
+    match &cli.dispatcher {
+        sub::Commands::Find {
+            rpc_url,
+            contract_address,
+        } => {
+            println! (
+                "rpc_url provided {}, Contract Address {}",
+                rpc_url, contract_address
+            );
+
+            let bytecode = rt
+                .block_on(queryeth::get_opcode(rpc_url, contract_address))
+                .unwrap()
+        }
+    }
 }
